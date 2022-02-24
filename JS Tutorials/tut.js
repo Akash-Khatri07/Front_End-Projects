96f4ab607fcff7b9e6931786c2b92ac12ca6942f
Akash Khatri <89866015+Akash-Khatri07@users.noreply.github.com>
2022-02-24 15:28:23 +0530

    Update tut.js

---
+++ JS Tutorials/tut.js
@@ -0,0 +1,27 @@
+//type conversion and type coercion
+console.log('welcome to tuts.js');
+let myVar;
+myVar = String(34);
+console.log(myVar, (typeof myVar));
+let booleanVar = String(true);
+console.log(booleanVar, (typeof booleanVar));
+
+let date = String(new Date());
+console.log(date,(typeof date));
+
+let arr = String([12,3,4,5]);
+console.log(arr.length,(typeof arr));     
+
+let i = 8;
+console.log(i)
+
+let stri = Number(false);
+stri = Number([2,3,4,5,6,7,8,9]);
+console.log(stri,(typeof  stri));
+let Number = '34';
+
+
+let mystr = Number("698");
+let mynum = 34;
+console.log(mystr + mynum);
+                                                                   
